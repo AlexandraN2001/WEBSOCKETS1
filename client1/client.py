9c448f6a7df10ba1aa4fad639e835a07a6176486
AlexandraN2001 <nacabrera@uce.edu.ec>
2024-12-11 08:21:16 -0500

    agg folder client

---
+++ client1/client.py
@@ -0,0 +1,15 @@
+import asyncio
+import websockets
+
+async def hello():
+    uri = "ws://xxx.xxx.xxx.xxx:8765"  # The URI must match the port on the server
+    try:
+        async with websockets.connect(uri) as websocket:
+            response = await websocket.recv()  # Receive the message from the server
+            print(f"Server response: {response}")
+    except Exception as e:
+        print(f"Connection error: {e}")
+
+if __name__ == "__main__":
+    asyncio.run(hello())
+    
